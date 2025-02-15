218b79ed36e6a9dbf9ed2f141154b7696f78efbb
Kasi David <kasidkure@gmail.com>
2025-02-15 07:48:30 +0100

    Sorted Regex

--- src/com/bloomtech/socialfeed/validators/EmailValidator.java
+++ src/com/bloomtech/socialfeed/validators/EmailValidator.java
@@ -2,16 +2,22 @@ package com.bloomtech.socialfeed.validators;
 
 import com.bloomtech.socialfeed.exceptions.EmailValidationException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidator implements Validator {
     public EmailValidator() {
     }
 
     private boolean isValidEmail(String email) {
         /*TODO: Validate that email begins with a letter or number, contains only letters, numbers, "." and "_", and
-        *that it follows the pattern of name@domain.identifier
-        */
+         *that it follows the pattern of name@domain.identifier
+         */
+        String emailRegex = "^[a-zA-Z\\d][a-z._A-Z\\d]+@[A-za-z]+\\.[A-Za-z]+$";
+        Pattern p = Pattern.compile(emailRegex);
 
-        return true;
+        Matcher m = p.matcher(email);
+        return m.matches();
     }
 
     @Override
--- src/com/bloomtech/socialfeed/validators/UserInfoValidator.java
+++ src/com/bloomtech/socialfeed/validators/UserInfoValidator.java
@@ -4,18 +4,46 @@ import com.bloomtech.socialfeed.exceptions.UserValidationException;
 import com.bloomtech.socialfeed.models.Role;
 import com.bloomtech.socialfeed.models.User;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserInfoValidator implements Validator {
 
     private boolean isValidUsername(String username) {
         //TODO: validate username begins with an uppercase letter, is at least 4 characters long, and only contains
         //letters, numbers, and underscores
-        return true;
+        String userNameRegex = "^[A-Z][A-Za-z0-9_]{3}[A-Za-z0-9_]*$";
+        Pattern p = Pattern.compile(userNameRegex);
+
+        Matcher m = p.matcher(username);
+        return m.matches();
+
     }
 
     private boolean isValidPassword(String password) {
         //TODO: validate password contains at least 8 characters, an uppercase, and a lowercase letter.
         //valid symbols include: !@#$%^&*
-        return true;
+        String passwordRegex = "(.{8}+)";
+        String upperRegex = "([A-Z]+)";
+        String lowerRegex = "([a-z]+)";
+        String digitRegex = "(\\d+)";
+        String symbolRegex = "([!@#$%^&*a-zA-Z\\d]+)";
+        Pattern pass = Pattern.compile(passwordRegex);
+        Pattern upper = Pattern.compile(upperRegex);
+        Pattern lower = Pattern.compile(lowerRegex);
+        Pattern digit = Pattern.compile(digitRegex);
+        Pattern symbol = Pattern.compile(symbolRegex);
+
+        Matcher mPass = pass.matcher(password);
+        Matcher mUpper = upper.matcher(password);
+        Matcher mLower = lower.matcher(password);
+        Matcher mDigit = digit.matcher(password);
+        Matcher mSymbol = symbol.matcher(password);
+
+        if (mPass.find() && mUpper.find() && mLower.find() && mDigit.find()) {
+            return mSymbol.find();
+        }
+        return false;
     }
 
     @Override
